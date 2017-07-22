b5f297e817f854aff3f6a05d6b5172b8d56d1217
sly01 <aerkoc01@gmail.com>
2017-07-22 11:05:50 +0000

    Nfs mount added

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,5 +2,13 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 	class { '::rsyslog::client': }
+	
+	include ::nfs::client
+
+	nfs::client::mount { '/scripts':
+		server => '192.168.33.50',
+		share  => '/test',
+		options => 'rw,vers=3',
+	}
 
 }
