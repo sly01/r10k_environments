c07f1913c9c51adc5c2a72d54bb0e7f2b5caa2ec
sly01 <aerkoc01@gmail.com>
2017-09-22 09:40:38 +0000

    Datavg profile added

---
+++ site/profile/manifests/datavg.pp
@@ -0,0 +1,12 @@
+class profile::datavg (
+  Hash $volume_groups = hiera(lvm::volume_groups::datavg),
+  Hash $logical_volumes = hiera(lvm::volume_groups::datavg::logical_volumes),
+  ) {
+  # It must be done in other way! Problem with physical servers
+  $default_dirs = [ '/bigtemp' ]
+  file { $default_dirs:
+    ensure => 'directory',
+  }
+  create_resources('lvm::volume_group', $volume_groups)
+  create_resources('lvm::logical_volume', $logical_volumes)
+}
