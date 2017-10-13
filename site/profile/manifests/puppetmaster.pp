70d2e8308a1c4459e96f7ca7258a6cc458d93648
sly01 <aerkoc01@gmail.com>
2017-10-13 09:48:38 +0000

    Puppetdb profile added

---
+++ site/profile/manifests/puppetmaster.pp
@@ -0,0 +1,6 @@
+class profile::puppetmaster {
+  # Configure puppetdb and its underlying database
+  class { 'puppetdb': }
+  # Configure the Puppet master to use puppetdb
+  class { 'puppetdb::master::config': }
+}
